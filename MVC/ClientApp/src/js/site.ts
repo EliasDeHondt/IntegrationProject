ba789c16c9ff8fac8df57d4eebe74a2a372e7a8c
Vincent Verboven <vincent.verboven@student.kdg.be>
2024-03-08 11:33:12 +0100

    MVC Webpack

---
+++ MVC/ClientApp/src/js/site.ts
@@ -0,0 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import '../css/site.css'
+
+console.log('The \'site\' bundle has been loaded!');
--- MVC/ClientApp/webpack.config.js
+++ MVC/ClientApp/webpack.config.js
@@ -1,25 +1,51 @@
 ﻿const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
     entry: {
-        site: './src/js/site.js'
+        site: './src/js/site.ts',
+        validation: './src/js/validation.ts',
+        index: './src/js/index.ts'
     },
     output: {
         filename: '[name].entry.js',
-        path: path.resolve(__dirname, '..', 'wwwroot', 'dist')
+        path: path.resolve(__dirname, '..', 'wwwroot', 'dist'),
+        clean: true
     },
     devtool: 'source-map',
     mode: 'development',
+    resolve: {
+        extensions: [".ts", ".js"],
+        extensionAlias: {'.js': ['.js', '.ts']}
+    },
     module: {
         rules: [
             {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                test: /\.ts$/i,
+                use: ["ts-loader"],
+                exclude: /node_modules/
+            },
+            {
+                test: /\.s?css$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif|webp)$/i,
+                type: "asset"
             },
             {
                 test: /\.(eot|woff(2)?|ttf|otf|svg)$/i,
                 type: 'asset'
             },
         ]
-    }
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "[name].css"
+        })
+    ]
 };
