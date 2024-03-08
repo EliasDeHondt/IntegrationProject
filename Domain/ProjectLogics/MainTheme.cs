854b8dde3d86204d5e5cba6a462cf8d1a50eb4ae
VeraWise <vera.wise@student.kdg.be>
2024-03-09 00:35:01 +0100

    Seed Maintheme and Project 1

--- Domain/ProjectLogics/MainTheme.cs
+++ Domain/ProjectLogics/MainTheme.cs
@@ -5,24 +5,26 @@ public class MainTheme : ITheme
     public long Id { get; set; }
     public string Subject { get; set; }
     public IEnumerable<Flow> Flows { get; set; }
-    public Project Project { get; set; }
+   // public Project Project { get; set; }
     public IEnumerable<SubTheme> Themes { get; set; }
 
+    //public MainTheme(long id, string subject, IEnumerable<Flow> flows, Project project, IEnumerable<SubTheme> themes)
     public MainTheme(long id, string subject, IEnumerable<Flow> flows, Project project, IEnumerable<SubTheme> themes)
     {
         Id = id;
         Subject = subject;
         Flows = flows;
-        Project = project;
+        //Project = project;
         Themes = themes;
     }
     
-    public MainTheme(long id, string subject, Project project)
+    //public MainTheme(long id, string subject, Project project)
+    public MainTheme(long id, string subject)
     {
         Id = id;
         Subject = subject;
         Flows = new List<Flow>();
-        Project = project;
+        //Project = project;
         Themes = new List<SubTheme>();
     }
 }
