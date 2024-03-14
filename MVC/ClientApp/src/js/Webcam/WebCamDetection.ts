33d42d72db1e1d3f5d1f29b338ac6155520ae540
Meastro85 <verboven85@gmail.com>
2024-03-15 00:23:21 +0100

    Person gets detected

--- MVC/ClientApp/src/js/Webcam/WebCamDetection.ts
+++ MVC/ClientApp/src/js/Webcam/WebCamDetection.ts
@@ -1,8 +1,7 @@
 ﻿import * as tf from "@tensorflow/tfjs";
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 
-console.log(tf.version)
-
+console.log(tf.version);
 const FPS: number = 30;
 const height: number = 480;
 const width: number = 640;
@@ -13,35 +12,51 @@ ct.width = video.videoWidth;
 ct.height = video.videoHeight;
 let canvas: HTMLCanvasElement = document.getElementById("canvasCV") as HTMLCanvasElement;
 let context: CanvasRenderingContext2D | null = ct.getContext("2d");
+let liveview: HTMLDivElement = document.getElementById("liveview") as HTMLDivElement;
 
 video.disablePictureInPicture = true;
 
-async function main() {
-    const model = await cocoSsd.load();
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-        .then(stream => {
-            video.srcObject = stream;
-            video.onloadedmetadata = () => {
-                video.play();
-                detectPeople();
-            }
-        })
-        .catch(function (err) {
-            console.log("An error occurred! " + err);
-        });
+const model = await cocoSsd.load();
+navigator.mediaDevices.getUserMedia({video: true, audio: false})
+    .then(stream => {
+        video.srcObject = stream;
+        video.onloadedmetadata = () => {
+            video.play().then(() => {
+                ct.width = video.videoWidth;
+                ct.height = video.videoHeight;
+            });
+            predictWebcam();
+        }
+    })
+    .catch(function (err) {
+        console.log("An error occurred! " + err);
+    });
 
-    async function detectPeople() {
-        const predictions = await model.detect(video);
-        context?.clearRect(0, 0, canvas.width, canvas.height);
+let children:any[] = [];
 
+function predictWebcam(){
+    context?.drawImage(video, 0, 0, ct.width, ct.height);
+    model.detect(video).then(predictions => {
+        context?.clearRect(0, 0, ct.width, ct.height);
+        for(let i = 0; i < children.length; i++){
+            liveview.removeChild(children[i])
+        }
+        children.splice(0);
         predictions.forEach(prediction => {
-            if (prediction.class === "person") {
-                const box = prediction.bbox;
+            if(prediction.score > 0.66 && prediction.class === "person"){
+                /*const highlighter = document.createElement('div');
+                highlighter.setAttribute('class','highlighter');
+                highlighter.style.left = prediction.bbox[0] + 'px';
+                highlighter.style.top = prediction.bbox[1] + 'px';
+                highlighter.style.width = prediction.bbox[2] + 'px';
+                highlighter.style.height = prediction.bbox[3] + 'px';
+                
+                liveview.appendChild(highlighter);
+                children.push(highlighter);*/
+                let box = prediction.bbox;
                 context?.strokeRect(box[0], box[1], box[2], box[3]);
             }
-        });
-        requestAnimationFrame(detectPeople);
-    }
+        })
+        window.requestAnimationFrame(predictWebcam);
+    })
 }
-
-main()
