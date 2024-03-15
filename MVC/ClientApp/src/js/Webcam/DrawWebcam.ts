55bf09c36fef121c978480209b1808ff3b12c1fc
Vincent Verboven <vincent.verboven@student.kdg.be>
2024-03-15 12:15:05 +0100

    Trying to load webcam async

---
+++ MVC/ClientApp/src/js/Webcam/DrawWebcam.ts
@@ -0,0 +1,5 @@
+﻿function drawWebcam(ctxVideo: CanvasRenderingContext2D, video: HTMLVideoElement, detectionCanvas: HTMLCanvasElement){
+    while(true){
+        ctxVideo.drawImage(video, 0, 0, detectionCanvas.width, detectionCanvas.height);
+    }
+}
--- MVC/ClientApp/src/js/Webcam/WebCamDetection.ts
+++ MVC/ClientApp/src/js/Webcam/WebCamDetection.ts
@@ -1,56 +1,70 @@
 ﻿import * as tf from "@tensorflow/tfjs";
 import * as cocoSsd from '@tensorflow-models/coco-ssd';
 
-let video: HTMLVideoElement = document.getElementById("vidInput") as HTMLVideoElement;
-let ct: HTMLCanvasElement = document.getElementById("canvasTransfer") as HTMLCanvasElement;
-let context: CanvasRenderingContext2D= ct.getContext("2d") ?? new CanvasRenderingContext2D();
-let liveview: HTMLDivElement = document.getElementById("liveview") as HTMLDivElement;
+const video: HTMLVideoElement = document.getElementById("vidInput") as HTMLVideoElement;
+const detectionCanvas: HTMLCanvasElement = document.getElementById("canvasDetection") as HTMLCanvasElement;
+const videoCanvas: HTMLCanvasElement = document.getElementById("canvasVideo") as HTMLCanvasElement;
+const choiceCanvas: HTMLCanvasElement = document.getElementById("canvasChoices") as HTMLCanvasElement;
+const ctx: CanvasRenderingContext2D= detectionCanvas.getContext("2d") ?? new CanvasRenderingContext2D();
+const ctxVideo: CanvasRenderingContext2D= videoCanvas.getContext("2d") ?? new CanvasRenderingContext2D();
+const ctxChoice: CanvasRenderingContext2D= choiceCanvas.getContext("2d") ?? new CanvasRenderingContext2D();
+const videoDrawer = new Worker('DrawWebcam.ts');
 
 video.disablePictureInPicture = true;
 
-const model = await cocoSsd.load();
-navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(stream => {
-        video.srcObject = stream;
-        video.onloadedmetadata = () => {
-            video.play().then(() => {
-                ct.width = video.videoWidth;
-                ct.height = video.videoHeight;
-            });
-            predictWebcam();
-        }
-    })
-    .catch(function (err) {
-        console.log("An error occurred! " + err);
-    });
 
-let children:any[] = [];
-
-function predictWebcam(){
-    model.detect(video).then(predictions => {
-        context.clearRect(0, 0, ct.width, ct.height);
-        for(let i = 0; i < children.length; i++){
-            liveview.removeChild(children[i])
-        }
-        children.splice(0);
-        context.drawImage(video, 0, 0, ct.width, ct.height);
-        predictions.forEach(prediction => {
-            if(prediction.score > 0.66 && prediction.class === "person"){
-                /*const highlighter = document.createElement('div');
-                highlighter.setAttribute('class','highlighter');
-                highlighter.style.left = prediction.bbox[0] + 'px';
-                highlighter.style.top = prediction.bbox[1] + 'px';
-                highlighter.style.width = prediction.bbox[2] + 'px';
-                highlighter.style.height = prediction.bbox[3] + 'px';
+async function startPhysical(){
+    await tf.setBackend('webgl');
+    const model = await cocoSsd.load();
+    navigator.mediaDevices.getUserMedia({video: true, audio: false})
+        .then(stream => {
+            video.srcObject = stream;
+            video.onloadedmetadata = () => {
+                video.play().then(() => {
+                    detectionCanvas.width = video.videoWidth;
+                    detectionCanvas.height = video.videoHeight;
+                    videoCanvas.width = video.videoWidth;
+                    videoCanvas.height = video.videoHeight;
+                    choiceCanvas.width = video.videoWidth;
+                    choiceCanvas.height = video.videoHeight;
+                    drawChoiceBoundaries(2, detectionCanvas.width, detectionCanvas.height);
+                    videoDrawer.postMessage([ctxVideo, video, detectionCanvas]);
+                    predictWebcam();
+                });
                 
-                liveview.appendChild(highlighter);
-                children.push(highlighter);*/
-                let box = prediction.bbox;
-                context.strokeStyle = 'red';
-                context.strokeRect(box[0], box[1], box[2], box[3]);
             }
         })
-        
-        window.requestAnimationFrame(predictWebcam);
-    })
+        .catch(function (err) {
+            console.log("An error occurred! " + err);
+        });
+
+    function predictWebcam(){
+        model.detect(video).then(predictions => {
+            ctx.clearRect(0, 0, detectionCanvas.width, detectionCanvas.height);
+            predictions.forEach(prediction => {
+                if(prediction.score > 0.66 && prediction.class === "person"){
+                    let box = prediction.bbox;
+                    ctx.strokeStyle = 'red';
+                    ctx.strokeRect(box[0], box[1], box[2], box[3]);
+                }
+            })
+
+            window.requestAnimationFrame(predictWebcam);
+        })
+    }
+    
 }
+
+
+
+
+function drawChoiceBoundaries(choices: number, width: number, height: number){
+    let lineDiff: number = width/choices;
+    const rectWidth: number = 10;
+    ctxChoice.fillStyle = 'green';
+    for(let i = 1; i < choices; i++){
+        ctxChoice.fillRect((lineDiff * i) - (rectWidth/2), 0, rectWidth, height);
+    }
+}
+
+startPhysical().then(() => console.log("Object detection started"));
