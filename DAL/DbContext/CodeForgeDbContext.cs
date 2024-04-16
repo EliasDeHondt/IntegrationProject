11f350bad4507ed32329b4c88ada2ede4621b095
VeraWise <vera.wise@student.kdg.be>
2024-04-16 13:44:27 +0200

    Contact info: inputelement and button outlined

--- DAL/DbContext/CodeForgeDbContext.cs
+++ DAL/DbContext/CodeForgeDbContext.cs
@@ -162,6 +162,7 @@ public class CodeForgeDbContext : IdentityDbContext<IdentityUser>
         modelBuilder.Entity<Selection>().HasKey("FK_Selection_AnswerId", "FK_Selection_ChoiceId");
 
         modelBuilder.Entity<Respondent>().HasKey(respondent =>  respondent.RespondentId);
+        //modelBuilder.Entity<Facilitator>().HasBaseType<User>();
     }
 
     public bool CreateDatabase(bool dropDatabase)
