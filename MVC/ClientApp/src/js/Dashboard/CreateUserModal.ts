4cba2de4d243f29a595f72b030e86acf553b5a50
Meastro85 <verboven85@gmail.com>
2024-04-17 00:51:42 +0200

    Modal progress

--- MVC/ClientApp/src/js/Dashboard/CreateUserModal.ts
+++ MVC/ClientApp/src/js/Dashboard/CreateUserModal.ts
@@ -7,7 +7,26 @@ const CreateUserModal = new Modal(document.getElementById('CreateUserModal')!, {
 });
 
 const butCreateUser = document.getElementById("butCreateUser") as HTMLButtonElement;
+const butCloseCreateUserModal = document.getElementById("butCloseCreateUserModal") as HTMLButtonElement;
+const butCancelCreateUserModal = document.getElementById("butCancelCreateUserModal") as HTMLButtonElement;
+const butConfirmCreateUser = document.getElementById("butConfirmCreateUser") as HTMLButtonElement;
 
 butCreateUser.onclick = () => {
     CreateUserModal.show();
+}
+
+butCloseCreateUserModal.onclick = () => {
+    CreateUserModal.hide();
+}
+
+butCancelCreateUserModal.onclick = () => {
+    // Empty fields
+    
+    CreateUserModal.hide();
+}
+
+butConfirmCreateUser.onclick = () => {
+    // API call to create user
+    
+    CreateUserModal.hide();
 }
