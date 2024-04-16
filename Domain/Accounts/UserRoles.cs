40471c4c1e2d5c47a5de582ba6c2ac46829cbeb5
Vincent Verboven <vincent.verboven@student.kdg.be>
2024-04-16 12:09:18 +0200

    Authorization works, redirect to dashboard works.

---
+++ Domain/Accounts/UserRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain.Accounts;
+
+public static class UserRoles
+{
+    public const string SystemAdmin = "SystemAdmin";
+    public const string Facilitator = "Facilitator";
+    public const string Respondent = "Respondent";
+    public const string PlatformAdmin = "PlatformAdmin";
+}
