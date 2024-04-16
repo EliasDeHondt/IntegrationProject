c7ad4f408336069b984b94177bc895ada24477e3
Vincent Verboven <vincent.verboven@student.kdg.be>
2024-04-16 16:05:39 +0200

    Add user button works

---
+++ MVC/ClientApp/src/js/Dashboard/CreateUserModal.ts
@@ -0,0 +1,13 @@
+﻿import {Modal} from "bootstrap";
+
+const CreateUserModal = new Modal(document.getElementById('CreateUserModal')!, {
+    keyboard: false,
+    focus: true,
+    backdrop: "static"
+});
+
+const butCreateUser = document.getElementById("butCreateUser") as HTMLButtonElement;
+
+butCreateUser.onclick = () => {
+    CreateUserModal.show();
+}
