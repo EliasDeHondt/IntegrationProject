fcc0d8c328860b102e3c65a64431b23d92aa972b
Vincent Verboven <vincent.verboven@student.kdg.be>
2024-04-19 15:00:31 +0200

    validation works

--- MVC/ClientApp/src/js/Dashboard/CreateUserModal.ts
+++ MVC/ClientApp/src/js/Dashboard/CreateUserModal.ts
@@ -21,6 +21,10 @@ const inputEmail = document.getElementById("inputEmail") as HTMLInputElement;
 const inputPassword = document.getElementById("inputPassword") as HTMLInputElement;
 const selectProject = document.getElementById("selectProject") as HTMLSelectElement;
 
+const nameWarning = document.getElementById('nameWarning') as HTMLElement;
+const emailWarning = document.getElementById('emailWarning') as HTMLElement;
+const passwordWarning = document.getElementById('passwordWarning') as HTMLElement;
+
 let id = document.getElementById("platformId")!.textContent
 
 butCreateUser.onclick = () => {
@@ -38,8 +42,10 @@ butCancelCreateUserModal.onclick = () => {
 butConfirmCreateUser.onclick = (ev) => {
     ev.preventDefault()
     // API call to create user
-    if(radioFacilitator.checked) createFacilitator()
-    if(radioAdmin.checked) createAdmin()
+    if(validateForm()){
+        if(radioFacilitator.checked) createFacilitator()
+        else if(radioAdmin.checked) createAdmin()
+    }
 }
 
 radioAdmin.onchange = () => {
@@ -59,6 +65,7 @@ function clearModal(){
     inputName.value = "";
     inputEmail.value = "";
     inputPassword.value = "";
+    resetWarnings();
     CreateUserModal.hide();
 }
 
@@ -119,4 +126,49 @@ function fillDropdown(data: Project[]) {
         option.text = data[i].mainTheme.subject;
         selectProject.appendChild(option);
     }
+}
+
+function validateForm(){
+    let valid: boolean = true;
+    
+    
+    if (inputName.value.trim() === '') {
+        nameWarning.textContent = 'Please enter a name';
+        valid = false;
+    } else {
+        nameWarning.textContent = '';
+    }
+
+    let emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    
+    if (inputEmail.value.trim() === '') {
+        emailWarning.textContent = 'Please enter an email';
+        valid = false;
+    } else if(!emailRegex.test(inputEmail.value)) {
+        emailWarning.textContent = 'Please enter a valid email';
+        valid = false;
+    } else {
+        emailWarning.textContent = '';
+    }
+    
+    let passwordRegex: RegExp = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,}$/
+    
+    if (inputPassword.value.trim() === '') {
+        passwordWarning.textContent = 'Please enter a password';
+        valid = false;
+    } else if(!passwordRegex.test(inputPassword.value)) {
+        passwordWarning.textContent = 'The password needs to be 10 characters long and contain at least one uppercase letter, one lowercase letter, one number and one special character';
+        valid = false;
+    }
+    else {
+        passwordWarning.textContent = '';
+    }
+    
+    return valid;
+}
+
+function resetWarnings(){
+    nameWarning.textContent = '';
+    emailWarning.textContent = '';
+    passwordWarning.textContent = '';
 }
