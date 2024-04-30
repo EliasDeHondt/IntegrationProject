57c35f134b723bca11402b2362f3c6bf6f8787ab
VeraWise <vera.wise@student.kdg.be>
2024-04-30 14:53:21 +0200

    collapse werkt

--- MVC/ClientApp/src/js/Dashboard/Dashboard.ts
+++ MVC/ClientApp/src/js/Dashboard/Dashboard.ts
@@ -55,22 +55,36 @@ document.addEventListener('DOMContentLoaded', function() {
     const navigationBar = document.getElementById('navbar') as HTMLElement;
     const mainContent = document.getElementById('maincontent') as HTMLElement;
     const collapseButton = document.getElementById('collapseButton') as HTMLElement;
+    const languageButton = document.getElementById('language') as HTMLElement;
+    const accountname = document.getElementById('tohideAN') as HTMLElement;
+    const dashboard = document.getElementById('tohideD') as HTMLElement;
+    const statistics = document.getElementById('tohideS') as HTMLElement;
+    const signout = document.getElementById('tohideLO') as HTMLElement;
     const icon = document.getElementById('icon') as HTMLElement;
     
     collapseButton.addEventListener('click', function(e) {
         e.preventDefault();
-        //navigationBar.classList.toggle('collapsed');
+        navigationBar.classList.toggle('collapsed');
 
         // Toggle icon
         icon.classList.toggle('bi-caret-left-fill');
         icon.classList.toggle('bi-caret-right-fill');
     });
     
-    // collapseButton.addEventListener('click', function() {
-    //     if (navigationBar.classList.contains('collapsed')) {
-    //         mainContent.style.paddingRight = '50px'; // Adjust to the width of the collapsed navigation bar
-    //     } else {
-    //         mainContent.style.paddingRight = '800px'; // Adjust to the width of the expanded navigation bar
-    //     }
-    // });
+    collapseButton.addEventListener('click', function() {
+        if (navigationBar.classList.contains('collapsed')) {
+            languageButton.style.display = 'none';
+            accountname.style.display = 'none';
+            dashboard.style.display = 'none';
+            statistics.style.display = 'none';
+            signout.style.display = 'none';
+        }else{
+            languageButton.style.display = 'block';
+            accountname.style.display = 'block';
+            dashboard.style.display = 'block';
+            statistics.style.display = 'block';
+            signout.style.display = 'block';
+            mainContent.style.paddingRight = '0'; // terug naar origineel
+        }
+    });
 });
