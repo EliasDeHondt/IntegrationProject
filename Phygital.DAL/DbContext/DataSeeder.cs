492f354125f25a148ff0b482dd8001353e1b3448
Matthias Hendrickx <matthias.hendrickx.1@student.kdg.be>
2024-05-14 14:35:38 +0200

    dataseeding

--- Phygital.DAL/DbContext/DataSeeder.cs
+++ Phygital.DAL/DbContext/DataSeeder.cs
@@ -348,8 +348,186 @@ namespace Data_Access_Layer.DbContext
             choice18.NextStep = step7;
         }
 
+        public static void GenerateKdgSteps(CodeForgeDbContext dbContext, Flow flowKdg)
+        {
+            GenerateSingleQuestionsKdg(dbContext, flowKdg, 1, 1);
+            GenerateSingleQuestionsKdg(dbContext, flowKdg, 3, 2);
+            GenerateSingleQuestionsKdg(dbContext, flowKdg, 2, 3);
+            GenerateMultipleChoiceQuestionsKdg(dbContext, flowKdg, 3, 4);
+            GenerateMultipleChoiceQuestionsKdg(dbContext, flowKdg, 2, 5);
+            GenerateMultipleChoiceQuestionsKdg(dbContext, flowKdg, 1, 6);
+            GenerateRangeQuestionsKdg(dbContext, flowKdg, 7);
+            GenerateOpenQuestionsKdg(dbContext, flowKdg, 8);
+            
+            Image imageInfoKdg = new Image(ImageUrls.Elections);
+            Hyperlink hyperlinkInfoKdg = new Hyperlink("https://studentkdg.sharepoint.com/sites/intranet-nl/SitePages/verkiezingen-StuRa.aspx?from=SendByEmail&e=zIeoHeJOsUW7zyhNtRfGkQ&at=9");
+            
+            flowKdg.Steps.Add(new InformationStep(9, new List<InformationBase> {imageInfoKdg}, flowKdg));
+            flowKdg.Steps.Add(new InformationStep(10, new List<InformationBase> {hyperlinkInfoKdg}, flowKdg));
+        }
+        
+        private static void GenerateSingleQuestionsKdg(CodeForgeDbContext dbContext, Flow flow, int caseIndex, int stepNumber)
+        {
+            switch (caseIndex)
+            {
+                case 1:
+                    SingleChoiceQuestion singleQuestions1 = new SingleChoiceQuestion("Welke van de volgende gebieden zou jij prioriteit geven voor verbetering in onze hogeschool voor het komende academische jaar? Kies er een.");
+
+                    Choice choice1 = new Choice("Het verbeteren van de diensten voor studentenondersteuning", singleQuestions1);
+                    Choice choice2 = new Choice("Het bevorderen van de betrokkenheid van studenten en buitenschoolse activiteiten", singleQuestions1);
+                    Choice choice3 = new Choice("Het verbeteren van de faciliteiten en infrastructuur van de campus", singleQuestions1);
+                    Choice choice4 = new Choice("Het verbeteren van academische programma's en middelen", singleQuestions1);
+                    Choice choice5 = new Choice("Het waarborgen van studentenvertegenwoordiging en belangenbehartiging", singleQuestions1);
+                    Choice choice6 = new Choice("Het bevorderen van diversiteit en inclusieve initiatieven", singleQuestions1);
+
+                    dbContext.ChoiceQuestions.AddRange(singleQuestions1);
+                    dbContext.Choices.AddRange(choice1, choice2, choice3, choice4, choice5, choice6);
+
+                    // Add to Step
+                    QuestionStep step1 = new QuestionStep(stepNumber, singleQuestions1, flow);
+                    flow.Steps.Add(step1);
+                    dbContext.QuestionSteps.Add(step1);
+                    break;
+
+                case 2:
+                    SingleChoiceQuestion singleQuestions2 = new SingleChoiceQuestion("Welke kwaliteiten acht jij het belangrijkst voor een succesvolle vertegenwoordiger van de studentenraad? Kies er een.");
+
+                    Choice choice7 = new Choice("Leiderschap", singleQuestions2);
+                    Choice choice8 = new Choice("Communicatievaardigheden", singleQuestions2);
+                    Choice choice9 = new Choice("Samenwerking", singleQuestions2);
+                    Choice choice10 = new Choice("Probleemoplossende vaardigheden", singleQuestions2);
+                    Choice choice11 = new Choice("Toewijding aan het welzijn van de studenten", singleQuestions2);
+                    Choice choice12 = new Choice("Initiatief en drive", singleQuestions2);
+
+                    dbContext.ChoiceQuestions.AddRange(singleQuestions2);
+                    dbContext.Choices.AddRange(choice7, choice8, choice9, choice10, choice11, choice12);
+
+                    // Add to Step
+                    QuestionStep step2 = new QuestionStep(stepNumber, singleQuestions2, flow);
+                    flow.Steps.Add(step2);
+                    dbContext.QuestionSteps.Add(step2);
+                    break;
+
+                case 3:
+                    SingleChoiceQuestion singleQuestions3 = new SingleChoiceQuestion("Hoe zou jij de prioritering van de toewijzing van fondsen voor studentenactiviteiten en -evenementen zien? Kies er een.");
+
+                    Choice choice13 = new Choice("Gelijkmatig over alle soorten evenementen", singleQuestions3);
+                    Choice choice14 = new Choice("Gebaseerd op de voorkeuren en interesses van studenten", singleQuestions3);
+                    Choice choice15 = new Choice("Prioriteit geven aan grote jaarlijkse evenementen", singleQuestions3);
+                    Choice choice16 = new Choice("Ondersteuning van nieuwe en innovatieve initiatieven", singleQuestions3);
+                    Choice choice17 = new Choice("Fondsen toewijzen op basis van eerdere successen van evenementen", singleQuestions3);
+                    Choice choice18 = new Choice("Anders", singleQuestions3);
+
+                    dbContext.ChoiceQuestions.AddRange(singleQuestions3);
+                    dbContext.Choices.AddRange(choice13, choice14, choice15, choice16, choice17, choice18);
+
+                    // Add to Step
+                    QuestionStep step3 = new QuestionStep(stepNumber, singleQuestions3, flow);
+                    flow.Steps.Add(step3);
+                    dbContext.QuestionSteps.Add(step3);
+                    break;
+            }
+        }
+
+        private static void GenerateMultipleChoiceQuestionsKdg(CodeForgeDbContext dbContext, Flow flow, int caseIndex, int stepNumber)
+        {
+            switch (caseIndex)
+            {
+                case 1:
+                    MultipleChoiceQuestion multipleQuestions1 = new MultipleChoiceQuestion("Wat beschouw jij als de belangrijkste verantwoordelijkheden van een studentenraadslid?");
+
+                    Choice choice1 = new Choice("Vertegenwoordigen van studentenbelangen", multipleQuestions1);
+                    Choice choice2 = new Choice("Organiseren van evenementen en activiteiten", multipleQuestions1);
+                    Choice choice3 = new Choice("Bevorderen van diversiteit en inclusie op de campus", multipleQuestions1);
+                    Choice choice4 = new Choice("Bijdragen aan de academische besluitvorming", multipleQuestions1);
+                    Choice choice5 = new Choice("Samenwerken met de schooladministratie voor verbeteringen", multipleQuestions1);
+
+
+                    dbContext.ChoiceQuestions.AddRange(multipleQuestions1);
+                    dbContext.Choices.AddRange(choice1, choice2, choice3, choice4, choice5);
+
+                    // Add to Step
+                    QuestionStep step1 = new QuestionStep(stepNumber, multipleQuestions1, flow);
+                    flow.Steps.Add(step1);
+                    dbContext.QuestionSteps.Add(step1);
+                    break;
+
+                case 2:
+                    MultipleChoiceQuestion multipleQuestions2 = new MultipleChoiceQuestion("Welke eigenschappen acht jij essentieel voor een effectieve studentenraadsvertegenwoordiger?");
+
+                    Choice choice7 = new Choice("Leiderschap", multipleQuestions2);
+                    Choice choice8 = new Choice("Communicatievaardigheden", multipleQuestions2);
+                    Choice choice9 = new Choice("Samenwerking", multipleQuestions2);
+                    Choice choice10 = new Choice("Probleemoplossend vermogen", multipleQuestions2);
+                    Choice choice11 = new Choice("Toewijding aan het welzijn van de studenten", multipleQuestions2);
+                    Choice choice12 = new Choice("Initiatief tonen en vastberadenheid", multipleQuestions2);
+
+
+                    dbContext.ChoiceQuestions.AddRange(multipleQuestions2);
+                    dbContext.Choices.AddRange(choice7, choice8, choice9, choice10, choice11, choice12);
+
+                    // Add to Step
+                    QuestionStep step2 = new QuestionStep(stepNumber, multipleQuestions2, flow);
+                    flow.Steps.Add(step2);
+                    dbContext.QuestionSteps.Add(step2);
+                    break;
+
+                case 3:
+                    MultipleChoiceQuestion multipleQuestions3 = new MultipleChoiceQuestion("Op welke gebieden zou jij als studentenraadslid prioriteit geven aan verbeteringen binnen de school?");
+
+                    Choice choice13 = new Choice("Verbeteren van studie- en academische ondersteuningsdiensten", multipleQuestions3);
+                    Choice choice14 = new Choice("Bevorderen van studentenwelzijn en geestelijke gezondheid", multipleQuestions3);
+                    Choice choice15 = new Choice("Uitbreiden van de betrokkenheid van studenten bij de gemeenschap", multipleQuestions3);
+                    Choice choice16 = new Choice("Verbeteren van faciliteiten en voorzieningen op de campus", multipleQuestions3);
+                    Choice choice17 = new Choice("Bevorderen van duurzaamheid en milieubewustzijn", multipleQuestions3);
+
+                    dbContext.ChoiceQuestions.AddRange(multipleQuestions3);
+                    dbContext.Choices.AddRange(choice13, choice14, choice15, choice16, choice17);
+
+                    // Add to Step
+                    QuestionStep step3 = new QuestionStep(stepNumber, multipleQuestions3, flow);
+                    flow.Steps.Add(step3);
+                    dbContext.QuestionSteps.Add(step3);
+                    break;
+            }
+        }
+
+
+        private static void GenerateRangeQuestionsKdg(CodeForgeDbContext dbContext, Flow flow, int stepNumber)
+        {
+            RangeQuestion rangeQuestion = new RangeQuestion("Studenten zouden meer invloed moeten hebben binnen het schoolbestuur");
+            
+            Choice choice1 = new Choice("Zeker niet", rangeQuestion);
+            Choice choice2 = new Choice("Waarschijnlijk niet", rangeQuestion);
+            Choice choice3 = new Choice("Onzeker", rangeQuestion);
+            Choice choice4 = new Choice("Waarschijnlijk wel", rangeQuestion);
+            Choice choice5 = new Choice("Zeker wel", rangeQuestion);
+            
+            dbContext.ChoiceQuestions.AddRange(rangeQuestion);
+            dbContext.Choices.AddRange(choice1, choice2, choice3, choice4, choice5);
+
+            // Add to step
+            QuestionStep step1 = new QuestionStep(stepNumber, rangeQuestion, flow);
+            flow.Steps.Add(step1);
+            dbContext.QuestionSteps.Add(step1);
+        }
+
+        private static void GenerateOpenQuestionsKdg(CodeForgeDbContext dbContext, Flow flow, int stepNumber)
+        {
+            OpenQuestion openQuestion = new OpenQuestion("Leg in je eigen woorden uit wat jij zou willen veranderen aan KdG");
+            
+            QuestionStep step = new QuestionStep(stepNumber, openQuestion, flow);
+            flow.Steps.Add(step);
+            dbContext.OpenQuestions.Add(openQuestion);
+            dbContext.QuestionSteps.Add(step);
+        }
+
         public static void Seed(CodeForgeDbContext dbContext)
         {
+            //=======================
+            // Create Main Themes & Sub Themes
+            //=======================
+            
             // Create Main Themes & Sub Themes (1)
             MainTheme mainTheme1 = new MainTheme("Local Elections");
             SubTheme subTheme1 = new SubTheme("Causes", mainTheme1);
@@ -371,20 +549,32 @@ namespace Data_Access_Layer.DbContext
             MainTheme mainTheme3 = new MainTheme("Technology");
             SubTheme subTheme4 = new SubTheme("Impact On Society", mainTheme3);
             Flow flow4 = new Flow(FlowType.Linear, subTheme4);
+            
+            // Create Main Themes & Sub Themes (4)
+            MainTheme mainTheme4 = new MainTheme("Studenten Verkiezingen");
+            SubTheme subTheme5 = new SubTheme("Verkiezingen Campus Groenplaats", mainTheme4);
+            Flow flow5 = new Flow(FlowType.Circular, subTheme5);
+            Flow flow6 = new Flow(FlowType.Linear, subTheme5);
 
+            //=======================
             // Add steps to flow 1 (Linear Flow)
+            //=======================
             GenerateSingleQuestions(dbContext, flow1, 1, 1);
             GenerateMultipleChoiceQuestions(dbContext, flow1, 1, 2);
             GenerateRangeQuestions(dbContext, flow1, 1, 3);
             GenerateOpenQuestions(dbContext, flow1, 1, 4);
 
+            //=======================
             // Add steps to flow 2 (Circular Flow)
+            //=======================
             GenerateSingleQuestions(dbContext, flow2, 2, 1);
             GenerateMultipleChoiceQuestions(dbContext, flow2, 2, 2);
             GenerateRangeQuestions(dbContext, flow2, 2, 3);
             GenerateOpenQuestions(dbContext, flow2, 2, 4);
             
+            //=======================
             // Add steps to flow 3 (Linear Flow)
+            //=======================
             GenerateSingleQuestions(dbContext, flow3, 3, 1);
             GenerateSingleQuestions(dbContext, flow3, 4, 2);
             GenerateOpenQuestions(dbContext, flow3, 5, 3);
@@ -393,9 +583,29 @@ namespace Data_Access_Layer.DbContext
             GenerateOpenQuestions(dbContext, flow3, 8, 6);
             GenerateOpenQuestions(dbContext, flow3, 9, 7);
             
+            //=======================
             // Add steps to flow 4 (Linear Flow)
+            //=======================
             GenerateConditonalQuestions(dbContext, flow4);
             
+            //=======================
+            // Add steps to flow 5 (Linear Flow)
+            //=======================
+            GenerateSingleQuestionsKdg(dbContext, flow5, 3, 1);
+            GenerateSingleQuestionsKdg(dbContext, flow5, 1, 2);
+            GenerateOpenQuestionsKdg(dbContext, flow5, 3);
+            
+            Image imageInfoKdg = new Image(ImageUrls.Elections);
+            Hyperlink hyperlinkInfoKdg = new Hyperlink("https://studentkdg.sharepoint.com/sites/intranet-nl/SitePages/verkiezingen-StuRa.aspx?from=SendByEmail&e=zIeoHeJOsUW7zyhNtRfGkQ&at=9");
+            
+            flow5.Steps.Add(new InformationStep(4, new List<InformationBase> {imageInfoKdg}, flow5));
+            flow5.Steps.Add(new InformationStep(5, new List<InformationBase> {hyperlinkInfoKdg}, flow5));
+            
+            GenerateKdgSteps(dbContext, flow6);
+            
+            //=======================
+            // Add Information Steps
+            //=======================
             Image imageInfo = new Image(ImageUrls.Elections);
             Video videoInfo = new Video("?"); // TODO: Google Bucket URL
             Hyperlink hyperlinkInfo = new Hyperlink("https://levuur.be/");
@@ -404,43 +614,55 @@ namespace Data_Access_Layer.DbContext
             flow1.Steps.Add(new InformationStep(6, new List<InformationBase> {hyperlinkInfo}, flow1));
             flow2.Steps.Add(new InformationStep(5, new List<InformationBase> {videoInfo}, flow2));
 
-            // Create Shared Platform & Projects
+            //=======================
+            // Create Shared Platforms & Projects
+            //=======================
             SharedPlatform sharedPlatform = new SharedPlatform("CodeForge", ImageUrls.Favicon);
             SharedPlatform sharedPlatform1 = new SharedPlatform("Karel de Grote", ImageUrls.kdg);
             SharedPlatform sharedPlatform2 = new SharedPlatform("Tree company", ImageUrls.tree);
             Project project1 = new Project(mainTheme1.Subject, mainTheme1, sharedPlatform);
             Project project2 = new Project(mainTheme2.Subject, mainTheme2, sharedPlatform);
             Project project3 = new Project(mainTheme3.Subject, mainTheme3, sharedPlatform);
+            Project project4 = new Project(mainTheme4.Subject, mainTheme4, sharedPlatform1);
 
-            ((SpAdmin)dbContext.Users.Single(u => u.Email == "Henk@CodeForge.com")).SharedPlatform = sharedPlatform;
-            ((SpAdmin)dbContext.Users.Single(u => u.Email == "CodeForge.noreply@gmail.com")).SharedPlatform =
-                sharedPlatform;
-            
-            dbContext.SharedPlatforms.RemoveRange(dbContext.SharedPlatforms.Where(p => p.OrganisationName == string.Empty));
-            dbContext.SharedPlatforms.AddRange(sharedPlatform1, sharedPlatform2);
-            // Create Project Organizers
+            //=======================
+            // Add Project Organizers
+            //=======================
             ProjectOrganizer projectOrganizer1 = new ProjectOrganizer(project1, (Facilitator)dbContext.Users.Single(user => user.Email == "Tom@CodeForge.com"));
-            ProjectOrganizer projectOrganizer2 = new ProjectOrganizer(project2,
-                (Facilitator)dbContext.Users.Single(user => user.Email == "Tom@CodeForge.com"));
+            ProjectOrganizer projectOrganizer2 = new ProjectOrganizer(project2, (Facilitator)dbContext.Users.Single(user => user.Email == "Tom@CodeForge.com"));
+            ProjectOrganizer projectOrganizer3 = new ProjectOrganizer(project4, (Facilitator)dbContext.Users.Single(user => user.Email == "Fred@kdg.be"));      
             
+            //=======================
             // Add users to shared platform
+            //=======================
             ((SpAdmin)dbContext.Users.Single(u => u.Email == "Henk@CodeForge.com")).SharedPlatform = sharedPlatform;
             ((SpAdmin)dbContext.Users.Single(u => u.Email == "CodeForge.noreply@gmail.com")).SharedPlatform = sharedPlatform;
             
+            ((SpAdmin)dbContext.Users.Single(u => u.Email == "Thomas@kdg.be")).SharedPlatform = sharedPlatform1;
+            ((SpAdmin)dbContext.Users.Single(u => u.Email == "kdg.noreply@kdg.be")).SharedPlatform = sharedPlatform1;
+                    
+            //=======================
             // Remove empty shared platforms
+            //=======================
             dbContext.SharedPlatforms.RemoveRange(dbContext.SharedPlatforms.Where(p => p.OrganisationName == string.Empty));
-            
+                    
+            //=======================
             // Add to database
+            //=======================
             dbContext.ProjectOrganizers.Add(projectOrganizer1);
             dbContext.ProjectOrganizers.Add(projectOrganizer2);
+            dbContext.ProjectOrganizers.Add(projectOrganizer3);
             sharedPlatform.Projects.Add(project1);
             sharedPlatform.Projects.Add(project2);
             sharedPlatform.Projects.Add(project3);
-            
+            sharedPlatform1.Projects.Add(project4);
+                    
+            //=======================
             // Save changes
+            //=======================
             dbContext.SaveChanges();
-            ((Facilitator)dbContext.Users.Single(user => user.Email == "Tom@CodeForge.com")).SharedPlatformId =
-                sharedPlatform.Id;
+            ((Facilitator)dbContext.Users.Single(user => user.Email == "Tom@CodeForge.com")).SharedPlatformId = sharedPlatform.Id;
+            ((Facilitator)dbContext.Users.Single(u => u.Email == "Fred@kdg.be")).SharedPlatformId = sharedPlatform1.Id;
             dbContext.ChangeTracker.Clear();
         }
     }
